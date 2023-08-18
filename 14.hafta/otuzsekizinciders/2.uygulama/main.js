8ea7464e4afbf892940db61b068894d562bc199e
Ezelhan Dikmenlioğlu <20060394@stu.omu.edu.tr>
2023-08-18 20:38:02 +0300

    finished

--- 14.hafta/otuzsekizinciders/2.uygulama/main.js
+++ 14.hafta/otuzsekizinciders/2.uygulama/main.js
@@ -8,7 +8,28 @@ $("#p1").click(function(){
 $("#p2").click(function(){
     setTimeout(function(){
         $("#giris").hide(2000).fadeOut(2000,function(){
-            $("kayit").show(2000).fadeIn(2000);
+            $("#kayit").show(2000).fadeIn(2000);
         });
     },500);
+});
+$(document).ready(function() {
+    $("#btn").click(function() {
+        var name = $("#kullanici").val();
+        var email = $("#email").val();
+        var password = $("#sifre").val();
+        var confirmPassword = $("#sifreTekraar").val();
+
+        if (name === "") {
+            $("#kullanici").attr("placeholder", "Ad boş bırakılmaz");
+        }
+        if (email === "") {
+            $("#email").attr("placeholder", "E-mail boş bırakılmaz");
+        }
+        if (password === "") {
+            $("#sifre").attr("placeholder", "Şifre boş bırakılmaz");
+        }
+        if (confirmPassword === "") {
+            $("#sifreTekraar").attr("placeholder", "Şifre tekrar boş bırakılmaz");
+        }
+    });
 });
